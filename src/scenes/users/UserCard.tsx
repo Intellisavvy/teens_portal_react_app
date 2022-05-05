cb53378476eaa57886e6e267eda3ba768bc77a53
Mani <mani@intellisavvy.com>
2022-05-04 22:37:56 -0500

    introduced MUI

--- src/scenes/users/UserCard.tsx
+++ src/scenes/users/UserCard.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@mui/material"
 
 export const UserCard = ()=> {
     return <div>
         user card
+        <Button variant="contained">Hello World</Button>
     </div>
 }
