48500523954a5b150d677779164f99e469cb3e7a
hareesh2504 <hareesh.molliti@intellisavvy.com>
2022-05-18 17:59:08 +0530

    Header & Footer amd Vendor & Parent forms

---
+++ src/testing/Navbars.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MainNavbar from './Components/Navbar/NavbarMain';
+import ParentNavbar from './Components/Navbar/NavbarParent';
+import TeenNavbar from './Components/Navbar/NavbarTeen';
+import VendorNavbar from './Components/Navbar/NavbarVendor';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TeenNavbar /> */}
+      {/* <MainNavbar /> */}
+      {/* <VendorNavbar /> */}
+      {/* <ParentNavbar /> */}
+    </div>
+  );
+}
+
+export default App;
