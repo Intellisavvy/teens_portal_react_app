aaa98cdb78202fa4c1d1d26484a5eca7f4c67074
sekhar-01 <sekharalusuri@gmail.com>
2022-06-21 17:55:31 +0530

    list of jobs cards added

---
+++ src/scenes/jobs/jobs.list.tsx
@@ -0,0 +1,12 @@
+import { JobCard } from "./jobCard"
+
+
+export const JobsList = () => {
+    return(
+        <JobCard/>
+    )
+    // const jobs = [1,2,3,4,5]
+    // return <div>{jobs.map(sim => {
+    //     return <JobCard />
+    // })}</div>
+}
