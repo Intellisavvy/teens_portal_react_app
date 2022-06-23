28c8ab2583e8ed16c6eb5caa71819e26dc1e7ff5
sekhar-01 <sekharalusuri@gmail.com>
2022-06-23 17:09:59 +0530

    removed unwanted methods and variables

--- src/scenes/customer/customer.register.tsx
+++ src/scenes/customer/customer.register.tsx
@@ -1,67 +1,33 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Png from './images/pic.png';
 import { useState } from 'react';
-import { useRef } from 'react';
 import TextField, { TextFieldProps } from '@mui/material/TextField';
 import './App.css';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
-import { GoogleMap, StandaloneSearchBox, Marker } from "@react-google-maps/api";
+import {  StandaloneSearchBox} from "@react-google-maps/api";
 import { LoadScript } from "@react-google-maps/api";
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import { configure } from '@testing-library/react';
-import { Link } from 'react-router-dom';
 import ReactInputMask from 'react-input-mask';
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
-
-let markerArray = [];
 const lib = ["places"];
-const key = "AIzaSyB5deXbkVfO-Vv-CJNW4asdOEc8Bb_Bbe4";
+const key = "AIzaSyCFtEPoeZg_ktUzAubSjwtHLCQCQA2LAdo";
 export const CustomerRegistration = () => {
     const state = {
-        currentLocation: { lat: 0, lng: 0 },
         markers: [],
         bounds: null
     };
     const [searchBox, setSearchBox ] = useState('');
-
     const onMapLoad = (map: object) => {
-        navigator?.geolocation.getCurrentPosition(
-            ({ coords: { latitude: lat, longitude: lng } }) => {
-                const pos = { lat, lng };
-                setState({ currentLocation: pos });
-            }
-        );
         google.maps.event.addListener(map, "bounds_changed", () => {
-            console.log(map);
-           
+            console.log(map); 
         });
     };
-
     const onSBLoad = (ref: any) => {
         setSearchBox(ref)
     };
-
-    const onPlacesChanged = () => {
-        
+    const onPlacesChanged = () => {    
     };
-
-
     const name= "^[A-Za-z]+$";
     const mail = /^[^\s@]+@[^\s@]+\.[^\s@]{3,}$/i;
     const ready="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{7})";
@@ -83,17 +49,6 @@ export const CustomerRegistration = () => {
     }
     const details=(e:any)=>{
         e.preventDefault();
-      if(user.length<6 || user.length>15){
-          <p>Contains characters min 6 max15</p>
-      }
-      else if(!user.match(name)){
-          <p>Enter only characters</p>
-      }
-      else if(user.length!=0){
-
-      }
-
-      
         setdisplay({
         user:'',
         number:'',
@@ -181,23 +136,14 @@ export const CustomerRegistration = () => {
                     { (code.length>5)&& code.length!=0 ?<p style={{color:'red'}}>Enter correct  Zip code</p>:null} 
                  </div>
                  <div>
-                    <Checkbox {...label} required/>By clicking, I accept TERMS & CONDITIONS 
-                 </div><br/>
-                 <div >
-                    <Button variant="contained" type={'submit'} sx={{marginLeft:'20rem',height:'3rem'}} >Register</Button>
+                    <Checkbox required/>By clicking, I accept TERMS & CONDITIONS 
                  </div>
-                
+                 <div className='bit'>
+                    <Button variant="contained"  className='button'>Register</Button>
+                 </div>
                      </form>
                      <p className='left'>Already a user?  SignIN </p>
-
       </Box>
        
         </div>
-}
-function setState(arg0: { currentLocation: { lat: number; lng: number; }; }) {
-    throw new Error("Function not implemented.");
-}
-
-function render() {
-    throw new Error("Function not implemented.");
 }
